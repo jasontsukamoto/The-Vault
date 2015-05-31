ac476c2745acf8fc1abe2b02ba5471fe2ffaa989
Jason Tsukamoto <jasontsukamoto@gmail.com>
2015-05-30 16:53:17 -1000

    completed the vault exercise

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+  var storage = {};
+  return {
+    setValue : function(key, value) {
+      storage.key = key;
+      storage.value = value;
+    },
+    getValue : function(key) {
+      if(key === undefined) {
+        return null;
+      } else if (key === storage.key) {
+        return storage.value;
+      } else {
+        return null;
+      }
+    }
+  };
 };
